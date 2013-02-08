434a02d2cc70ab58479a2c67d7045dc15d1c5ccd
Todd Parsnick <tparsnick@psg.ucsf.edu>
2013-02-08 12:36:01 -0800

    Removed defaultalbum art setting and using spacer.gif instead

--- redcap_v4.15.2/Resources/js/customize_yahoo_mediaplayer.js
+++ redcap_v4.15.2/Resources/js/customize_yahoo_mediaplayer.js
@@ -7,13 +7,15 @@ This file of customizations must come before  <script type="text/javascript" src
 stop the player from advancing to the next track in the playlist.  
 We do not want participants getting another sound track automatically; this could be confusing.
 
-Aslo include a default gif in the player which overrides the play.gif, so as to avoid confusion.  We don't want users clicking a gif that does nothing.
+Also include a default gif in the player which overrides the play.gif, so as to avoid confusion.  We don't want users clicking a gif that does nothing.
+Note that defaultalbumart does not work when an image is already in the anchor tag.  So I didn't bother setting it.  However, I put the image I want as
+the first image tag in anchor, like so
+Regular rhythm <a href="audio/Regular_Normal_Heart_Beat.mp3"><img src="images/spacer.gif" style="display:none" /><img src="images/play_gray.png" /></a>
 
 Make the volume max loud by default
 */
 var YWPParams = 
 {
     autoadvance: false,
-    defaultalbumart: 'https://www.facebook.com/images/spacer.gif',
     volume: 1.0
 };
