6ffa04046d13fd171131de0463442dbea6489748
Todd Parsnick <tparsnick@psg.ucsf.edu>
2013-02-08 12:02:52 -0800

    Made the volume in yahooplayer max by default

--- redcap_v4.15.2/Resources/js/customize_yahoo_mediaplayer.js
+++ redcap_v4.15.2/Resources/js/customize_yahoo_mediaplayer.js
@@ -8,9 +8,12 @@ stop the player from advancing to the next track in the playlist.
 We do not want participants getting another sound track automatically; this could be confusing.
 
 Aslo include a default gif in the player which overrides the play.gif, so as to avoid confusion.  We don't want users clicking a gif that does nothing.
+
+Make the volume max loud by default
 */
 var YWPParams = 
 {
     autoadvance: false,
-    defaultalbumart: 'https://www.facebook.com/images/spacer.gif'
+    defaultalbumart: 'https://www.facebook.com/images/spacer.gif',
+    volume: 1.0
 };
